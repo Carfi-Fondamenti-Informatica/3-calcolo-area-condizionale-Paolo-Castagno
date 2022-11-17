485d9f643f111bd0a41159076db3b236992070e5
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-11-17 13:48:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
-   return 0;
+   float a=0,b=0,c=0;
+    int opzione=0;
+    cin>>a>>b>>opzione;
+    switch (opzione){
+        case 0:
+            c=a*b/2;
+            cout<<c;
+            break;
+        case 1:
+            c=a*a;
+            cout<<c;
+            break;
+        case 2:
+            c=a*b;
+            cout<<c;
+            break;
+        default:
+            cout<<"opzione non valida";
+        break;
+    }
+            
+    return 0;
 }
+
